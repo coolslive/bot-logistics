584550e61028ef32f8d8de8bcd185337e0c6c4be
coolslive <coolslive@gmail.com>
2024-01-28 23:40:06 +0300

    pandas

--- bot.py
+++ bot.py
@@ -1,8 +1,14 @@
+import pandas as bd
 from natasha import MorphVocab, AddrExtractor
 
 
 morph_vocab = MorphVocab()
 addr_extractor = AddrExtractor(morph_vocab)
+
+#df = pd.read_excel('ap.xlsd')
+#cell_value = df.iat[0, 0] # Получить значение первой ячейки (0, 0)
+#df.insert(0, "New column", "данные") добавляем новый столбец
+#df.to_excel("output-ap.xlsd", index=False) сохранение измененых данных
 
 text = "354002, Краснодарский край, Сочи г, Транспортная ул, дом № 2а"
 
