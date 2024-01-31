ecada2314f86e03c1944480639703198ab3304ca
coolslive <coolslive@gmail.com>
2024-02-01 00:03:45 +0300

    addr-work

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ addr_extractor = AddrExtractor(morph_vocab)
 #df.insert(0, "New column", "данные") добавляем новый столбец
 #df.to_excel("output-ap.xlsd", index=False) сохранение измененых данных
 
-text = "354002, Краснодарский край, Сочи г, Транспортная ул, дом № 2а"
+text = "354002, 186137, Карелия Респ, Пряжинский р-н, оренбург, Эссойла п, Центральная ул, дом № 7А"
 
 matches = addr_extractor(text)
 
